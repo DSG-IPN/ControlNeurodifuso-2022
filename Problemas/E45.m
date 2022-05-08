62a556c3d9f2e605f03549a71655dbb6bd5fc1a4
AriManuelGM <100552648+AriManuelGM@users.noreply.github.com>
2022-05-07 22:10:25 -0500

    Add files via upload

---
+++ Problemas/E45.m
@@ -0,0 +1,22 @@
+close all
+clear all
+clc
+W0=rand(2,1);
+b0=rand(1);
+t=[1 0 1 0];
+p1=[-1;1];
+p2=[-1;-1];
+p3=[1;-1];
+p4=[1;1];
+p=[p1 p2 p3 p4];
+Iteraciones = [W0;b0];
+n=1;
+while n<10
+    for i=1:4
+        [W,b] = algo_per_ite(W0,b0,p(1:2,i),t(1,i))
+        W0=W;
+        b0=b;
+        Iteraciones = [Iteraciones [W0;b0]]
+    end
+    n=n+1;
+end
